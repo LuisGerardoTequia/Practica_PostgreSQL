9642a1fff48506e154a306452bc4e02696d3499b
Luis Gerardo Tequia Tequia <150386327+LuisGerardoTequia@users.noreply.github.com>
2024-11-15 15:36:16 -0500

    Delete create_materialized_view.sql

--- create_materialized_view.sql
+++
@@ -1,9 +0,0 @@
--- Crear una vista materializada para análisis de ventas por región
-CREATE MATERIALIZED VIEW mv_sales_summary AS
-SELECT c.region, COUNT(s.sale_id) AS total_sales, SUM(s.total_amount) AS total_revenue
-FROM sales s
-JOIN customers c ON s.customer_id = c.customer_id
-GROUP BY c.region;
-
--- Refrescar la vista materializada
-REFRESH MATERIALIZED VIEW mv_sales_summary;
